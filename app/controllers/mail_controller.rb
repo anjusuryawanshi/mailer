4ca3ed4377032d34a83bedbe1ebc6821ca6bc807
Anju Suryawanshi <anjali.1585@gmail.com>
2014-07-11 10:58:01 -0700

    adding POST method in mail controller

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,11 +1,17 @@
-require 'mail'
-
 class MailController < ApplicationController
 
   respond_to :json
 
   def index
-    @mail = Mail.new("this is a body")
+    respond_with Mail.all
+    Mail.instance_methods(false).map { |m|
+      puts Mail.instance_method(m).source_location.first
+    }.uniq
+  end
+
+  def create
+    @mail = Mail.new(params[:mail])
+    puts @mail
     respond_with @mail
   end
 
